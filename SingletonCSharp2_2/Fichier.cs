f76fdf4221bde318a908fa96e1b9c3bed1f66924
Francis <super_f_qc@hotmail.com>
2015-10-09 16:02:24 -0400

    init

---
+++ SingletonCSharp2_2/Fichier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SingletonCSharp2_2
+{
+    sealed class Fichier
+    {
+        private static StreamWriter file = new StreamWriter("UnFichier.txt");
+
+        static Fichier Instance =new Fichier();
+
+        private Fichier()
+        {
+
+        }
+
+        public static Fichier getInstance()
+        {
+           return Instance;
+        }
+
+        public void write(String str)
+        {
+            file.WriteLineAsync(str);
+            file.Flush();
+        }
+    }
+}
---
+++ SingletonCSharp2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonCSharp2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fichier f = Fichier.getInstance();
+            f.write("dfdfdfdfdfssdf");
+        }
+    }
+}
